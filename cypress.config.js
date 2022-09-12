5ee2cb6598b8ba9b6f86d7e71b707c50c638bfa2
Darya.Tarasiuk <tarasiuk.darya@gmail.com>
2022-09-12 18:26:45 +0300

    added config files into the project

---
+++ cypress.config.js
@@ -0,0 +1,15 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  projectId: 'zven1f',
+  defaultCommandTimeout: 20000,
+  e2e: {
+    baseUrl: 'https://store.google.com',
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+  env: {
+    apiURL: 'https://reqres.in'
+  }
+});
