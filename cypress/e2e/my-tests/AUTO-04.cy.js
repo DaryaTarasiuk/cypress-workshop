f25f9c8facb67a23a4c90c525ada72d81ba98872
Darya.Tarasiuk <tarasiuk.darya@gmail.com>
2022-09-19 17:30:40 +0300

    added changes into AUTO-04.cy.js

--- cypress/e2e/my-tests/AUTO-04.cy.js
+++ cypress/e2e/my-tests/AUTO-04.cy.js
@@ -1,21 +1,62 @@
+   import Chance from 'chance'
 describe ('Set Creating', ()=> {
 
 })
 
-    it(`Create set`, function  ()  {
-
-        const currencySet= new Set()
-        currencySet.add('USD')
-        currencySet.add('EU')
+    it(`Create set,tasks 1-5`, function  ()  {
+         let currencySet= new Set(['USD','EU','BYN'])
+        //add elements
         currencySet.add('BYN').add('RUB').add('USD')
-        //console.log(mySet)
 
-        currencySet.forEach(currencySet => {
-           cy.log(currencySet)
+
+
+        currencySet.forEach(function(value) {
+            cy.log(value);
 
         })
+        //checking for presence elements in the set
+        cy.log('Set has EU value: ' + currencySet.has('EU'));
+         //checking for absents elements in the set
+        cy.log('Set has NFT value: ' + currencySet.has('NFT'));
 
-})
+         //convert set to array
+        let currencyArray=Array.from(currencySet)
+        console.log([...currencyArray])
+
+
+        //output of a random value
+        chance.pickone(currencyArray)
+        cy.log('Random value - '+chance.pickone(['USD','EU','BYN','RUB']))
+
+
+        //output of a random values
+        cy.log('Currency Set size - '+currencySet.size)
+        chance.integer({min:1,max:4})
+        chance.pickset(currencyArray)
+        cy.log('Random values - '+chance.pickset(['USD', 'EU', 'BYN', 'RUB'], 2))
+    })
+
+   import{isSuperset,union,intersection,difference} from "D:/projects/cypress-workshop/cypress/Utils/helper";
+   it(`Create new sets, task 6`, function  () {
+       let setA = new Set([1,2,3,4]),
+        setB = new Set([2,3]),
+        setC = new Set([3,4,5,6])
+       console.log(setA,setB,setC)
+
+       isSuperset (setA,setB)
+       union (setA,setC)
+       intersection(setA,setC)
+       difference(setA,setC)
+
+
+       cy.log(isSuperset(setA,setB)),
+       cy.log(union(setA,setC)),
+       cy.log(intersection(setA,setC)),
+       cy.log(difference(setA,setC))
+
+
+   })
+
 
 
 
