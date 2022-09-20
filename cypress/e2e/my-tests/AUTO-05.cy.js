5341703b6ed0c6e1c7e39a68e4622dc2c9f1aa22
Darya.Tarasiuk <tarasiuk.darya@gmail.com>
2022-09-20 15:20:18 +0300

    created  AUTO-05.cy.js

---
+++ cypress/e2e/my-tests/AUTO-05.cy.js
@@ -0,0 +1,54 @@
+describe ('Array Creating', ()=> {
+
+})
+
+it(`Create Array`, function  ()  {
+    let planets= ([
+            {planet: "Mercury", radius: 2440, density: 5.43, distance: 0.395},
+            {planet: "Venus", radius: 6052, density: 5.24, distance: 0.723},
+            {planet: "Earth", radius: 6378, density: 5.52, distance: 1},
+            {planet: "Mars", radius: 3396, density: 3.93, distance: 1.53},
+            {planet: "Jupiter", radius: 71492, density: 1.33, distance: 5.21},
+            {planet: "Saturn", radius: 60268, density: 0.69, distance: 9.551},
+            {planet: "Uranus", radius: 25559, density: 1.27, distance: 19.213},
+            {planet: "Neptune", radius: 24764, density: 1.64, distance: 30.07}
+        ]
+    )
+
+
+   //add solarSystem
+
+
+   const solarSystem=planets.map(function(currentvalue){
+       return currentvalue +"solarSystem:true"
+   })
+      console.log(solarSystem)
+     console.log(planets)
+
+        //output planets Array to the console
+    planets.forEach(planet => {
+        cy.log(JSON.stringify(planet));
+    })
+//add a new element
+    let newLength = planets.push({planet: "SomeNewPlanet", radius: 24764, density: 1.64, distance: 30.07, solarSystem: false})
+
+//output new planets Array to the console
+    planets.forEach(planet => {
+        cy.log(JSON.stringify(planet));
+    })
+//sum the radiuse
+    const totalRadius=planets.reduce((acc,cur)=>{
+       return acc+cur.radius;
+    },0)
+    cy.log('Total radius is  ' + totalRadius)
+
+//planet with Distance>5
+    const PlanetsWithBigDistance = planets.filter((planets, index, arr) => {
+
+        return planets.distance > 5;
+    });
+
+    cy.log(' Planets with distance > 5 ' + PlanetsWithBigDistance)
+
+
+})
