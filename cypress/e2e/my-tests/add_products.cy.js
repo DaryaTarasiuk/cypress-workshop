016440acdf7f3f28750076a36be520dfc6364159
Darya.Tarasiuk <tarasiuk.darya@gmail.com>
2022-09-29 12:54:08 +0300

    added changes into the add_products.cy.js

--- cypress/e2e/my-tests/add_products.cy.js
+++ cypress/e2e/my-tests/add_products.cy.js
@@ -15,10 +15,23 @@ describe ('Test adding products to the user card', () => {
         //go to product page
         cy.contains ('Pixel 6a').should('exist').click()
         //click on Buy button
-       //cy.get('a[href="https://store.google.com/us/config/pixel_6a?hl=en-US"]').click()
-       //cy.get('[data-test="cta"]').click({ multiple: true })
-        //cy.contains('Buy').click({force:true})
+
         cy.get('button[aria-label="Buy Pixel 6a"]').eq(1).click()
-                })
+        // user select a carrier
+        cy.get('[aria-label="Charcoal"]').click()
+        //then user have an opportunity to choose a colour
+        cy.get('[aria-label="Select Charcoal"]').click()
+        //then the user skips the final question before adding the product to the cart
+        cy.get('[class="xyh6bd MaEKpb"]').click()
+       //then the user skips the final question before adding the product to the cart
+        cy.get('[class="IydZSc qewDlc"]').click()
+        //now user can add this product to the cart
+        cy.get('[data-test="add-to-cart"]').click()
+
+        //now we should make sure that the correct product is in the cart
+        cy.get('[aria-label="Go to shopping cart"]').click()
+        // now user can delete these products from cart
+        cy.get('[aria-label="Remove Pixel 6a 128GB Charcoal (Unlocked) from cart"]').click()
+    })
             })
 
